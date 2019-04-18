6eb6b6c6311c2bfe525a5f3158cf505e2c5be922
Tiago Ferrer <ferrertiago@gmail.com>
2019-04-17 21:53:52 -0300

    adicionando test para verificacao de lugares

--- src/test/java/br/com/caelum/ingresso/model/SessaoTest.java
+++ src/test/java/br/com/caelum/ingresso/model/SessaoTest.java
@@ -3,7 +3,11 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.LocalTime;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import br.com.caelum.ingresso.model.descontos.TipoDeDesconto;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -22,5 +26,30 @@ public class SessaoTest {
 		
 		Assert.assertEquals(somaDosPrecosDaSalaEFilme, sessao.getPreco());
 		
+	}
+
+	@Test
+	public void garanteQueOLugarA1EstaOcupadoEOsLugaresA2eA3estaolivres (){
+
+		Lugar a1 = new Lugar ("A",1);
+		Lugar a2 = new Lugar ("A",2);
+		Lugar a3 = new Lugar ("A",3);
+
+		Filme filme = new Filme("Titanic", Duration.ofMinutes(120),"Drama",BigDecimal.TEN);
+
+		Sala sala = new Sala("Sala 1", BigDecimal.ONE);
+
+		Sessao sessao = new Sessao(LocalTime.now(), sala, filme);
+
+		Ingresso ingresso = new Ingresso(sessao, TipoDeDesconto.BANCO, a1);
+
+		Set<Ingresso> ingressos = Stream.of(ingresso).collect(Collectors.toSet());
+
+		sessao.setIngressos(ingressos);
+
+		Assert.assertFalse(sessao.isDisponivel(a1));
+		Assert.assertTrue(sessao.isDisponivel(a2));
+		Assert.assertTrue(sessao.isDisponivel(a3));
+
 	}
 }
